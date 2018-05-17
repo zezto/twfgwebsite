c8029ff15c7cf96e916dcb1f727565ad1c135a1a
zezto <31606201+zezto@users.noreply.github.com>
2018-05-17 18:40:48 -0500

    url fix

--- home/urls.py
+++ home/urls.py
@@ -3,12 +3,6 @@ from . import views
 
 urlpatterns = [
     #homepage
-<<<<<<< HEAD
-    path('', views.index, name='index'),
-    path('es', views.spanish, name='spanish')
-    
-=======
     url(r'^', views.index, name='index'),
->>>>>>> 960b8690900046d4e0f7fd35ed32cf8712e10404
-
+    url(r'^/es$', views.spanish, name='spanish'),
 ]
