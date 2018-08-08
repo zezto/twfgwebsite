f1fb536fcd657aa514984ba9a9194d1300925cce
zezto <31606201+zezto@users.noreply.github.com>
2018-08-08 18:46:04 -0500

    fixed robot

--- home/urls.py
+++ home/urls.py
@@ -1,16 +1,16 @@
 from django.conf.urls import include, url
 from . import views
-
+from django.views.generic import TemplateView
 urlpatterns = [
-    #homepage
+    # homepage
     url(r'^/?$', views.index, name='index'),
     url(r'^es$', views.spanish, name='spanish'),
-    url(r'^extra$', views.extra,name='extra'),
-    url(r'^mas$', views.mas,name='mas'),
+    url(r'^extra$', views.extra, name='extra'),
+    url(r'^mas$', views.mas, name='mas'),
     url(r'^form$', views.form, name='form'),
     url(r'^prospect$', views.pros, name='client'),
     url(r'^forma$', views.forma, name='forma'),
-    url(r'^robots.txt$', views.robot, name='robot')
-
+    url(r'^robots.txt$', TemplateView.as_view(
+        template_name="robots.txt", content_type="text/plain"), name="robots_file")
 
 ]
